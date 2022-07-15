808a367cf5d15a3328dc755d824a048f67a8d23f
Veronika Savluk <s_veronica@ukr.net>
2022-07-15 22:06:13 +0300

    task 1 (finish)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // Створення і рендер розмітки на підставі масиву даних galleryItems і
 // наданого шаблону елемента галереї.
-
+// Розмітка елемента галереї
+// Посилання на оригінальне зображення повинно зберігатися в data - атрибуті source
+//  на елементі < img >, і вказуватися в href посилання.Не додавай інші HTML теги або
+// CSS класи, крім тих, що містяться в цьому шаблоні.
 const boxGallery = document.querySelector(".gallery");
 const addGalleryItems = galleryItems.map((item) =>
 `<div class="gallery__item">
@@ -17,17 +20,6 @@ const addGalleryItems = galleryItems.map((item) =>
 </div >`).join(" ");
 boxGallery.insertAdjacentHTML("beforeend", `${addGalleryItems}`);
 // Реалізація делегування на div.gallery і отримання url великого зображення.
-// const selectImage = (event) => {
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-
-//   const selectedImage = event.target.dataset.source;
-// };
-// const addOnGalleryItemsClick = boxGallery.addEventListener("click", selectImage);
-// const removeOnGalleryItemsClick = boxGallery.removeEventListener("click", selectImage);
-console.log(galleryItems);
-
 // Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
 // Використовуй CDN сервіс jsdelivr і додай у проект посилання на
 // мініфіковані(.min) файли бібліотеки.
@@ -36,30 +28,25 @@ console.log(galleryItems);
 // Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
 // Використовуй готову розмітку модального вікна із зображенням
 //  з прикладів бібліотеки basicLightbox.
-
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data - атрибуті source
-//  на елементі < img >, і вказуватися в href посилання.Не додавай інші HTML теги або
-// CSS класи, крім тих, що містяться в цьому шаблоні.
-
-// <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>
-// // Зверни увагу на те, що зображення обгорнуте посиланням,
-// // отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку.
-// Заборони цю поведінку за замовчуванням.
-
-// Закриття з клавіатури
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання,
-// але буде хорошою додатковою практикою.
+const selectImage = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}">
+        </div>
+`, {
+    onShow: (instance) => {
+        instance.element().querySelector('img').onclick = instance.close;
+    }
+});
+  instance.show();
+};
+boxGallery.addEventListener("click", selectImage);
+// boxGallery.addEventListener("keydown", selectImage);
+console.log(galleryItems);
 
 // Додай закриття модального вікна після натискання клавіші Escape.
 // Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
